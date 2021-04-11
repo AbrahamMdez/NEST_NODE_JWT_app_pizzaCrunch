0cf47ebeb476163c970b7e8d19bd86f967848a2a
Abraham <nencho_1@hotmail.com>
2021-04-11 08:31:00 +0200

    created interface, schema and conection with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule, 
+    MongooseModule.forRoot('mongodb://localhost/pizza-crunch')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from '../../dist/products/interfaces/products.interface';
+import { CreateProductDTO } from './dto/products.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    
+}
