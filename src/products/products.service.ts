b87e444410909fe36f593c78b96c6e6b0793d10e
Abraham <nencho_1@hotmail.com>
2021-04-11 08:57:11 +0200

    done all http methods to the service product module

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Product } from '../../dist/products/interfaces/products.interface';
+import { Product } from './interfaces/products.interface';
 import { CreateProductDTO } from './dto/products.dto';
 
 @Injectable()
 export class ProductsService {
-    
-}
+
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>) {}
+
+    async getProducts(): Promise<Product[]> {
+        const getAllProducts = await this.productModel.find();
+        return getAllProducts;
+    }
+
+    async getProduct(productID: string): Promise<Product> {
+        const getOneProduct = await this.productModel.findById(productID);
+        return getOneProduct;
+    }
+
+    async createProduct(createProductDTO: CreateProductDTO): Promise<Product> {
+        const newProduct = new this.productModel(createProductDTO);
+        return await newProduct.save(); 
+    }
+
+    async updateProduct(productID: string, createProductDTO: CreateProductDTO): Promise<Product> {
+        const productToUpdate = await this.productModel.findByIdAndUpdate(productID, createProductDTO,
+            { new: true });
+        return productToUpdate;
+    }
+
+    async deleteProduct(productID: string): Promise<Product> {
+        const productToDelete = await this.productModel.findByIdAndDelete(productID);
+        return productToDelete;
+    }
+};
