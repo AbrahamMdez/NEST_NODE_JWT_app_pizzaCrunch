84f723769e8a9e93c571979c804fe7ea1872c897
Abraham <nencho_1@hotmail.com>
2021-04-25 10:34:04 +0200

    created config module and config to dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/pizza-crunch', {
       useFindAndModify: false
-    })
+    }),
+    ConfigModule
   ],
   controllers: [AppController],
   providers: [AppService],
